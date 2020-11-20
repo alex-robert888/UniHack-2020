c5ad00408f6c343c298bd6a2cb4bb60b38d09a9b
Alexandru Robert <robertalexandru2712@gmail.com>
2020-11-20 15:30:42 +0200

    Added dots

---
+++ client/src/components/base/BaseMultipageCard.js
@@ -0,0 +1,11 @@
+import './BaseMultipageCard.scss';
+
+const BaseMultipageCard = () => {
+    return (
+        <article className='base-multipage-card'>
+            
+        </article>
+    );
+}
+
+export default BaseMultipageCard;
