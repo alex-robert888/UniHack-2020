4b724e5e9e22767b75bfcc21643ac47d8bf3efda
Alexandru Robert <robertalexandru2712@gmail.com>
2020-11-20 23:05:54 +0200

    ready to load data in contex and db

--- client/src/components/base/BaseInputText.js
+++ client/src/components/base/BaseInputText.js
@@ -1,11 +1,11 @@
 import './BaseInputText.scss';
 import '../../style/classes.scss';
 
-const BaseInputText = ({title, icon, type}) => {
+const BaseInputText = ({title, icon, type, valueUpdated}) => {
     return (
         <article className='base-input-text glb-base-input-component'>
             <label htmlFor="input-name" className="glb-base-label">{title}</label>
-            <input type={type} name="input-name" className="glb-base-input" required>
+            <input type={type} name="input-name" className="glb-base-input" required onChange={ event => valueUpdated(event.target.value) }>
                 {/* <img src={icon} alt="icon"/> */}
             </input>
         </article>
--- client/src/components/the-auth-page/TheSignupForm.js
+++ client/src/components/the-auth-page/TheSignupForm.js
@@ -7,26 +7,72 @@ import { Fragment, useState } from 'react';
 import '../../style/classes.scss'
 
 const TheSignupForm = () => {
+    let [userSignupData, setUserSignUpData] = useState({
+        type: '',
+        fullName: '',
+        email: '',
+        telephone: '',
+        gender: '',
+        dateOfBirth: '',
+        username: '',
+        password: '',
+        confirmedPassword: ''
+    });
+
+    let [type, setType] = useState('');
+    let [fullName, setFullName] = useState('');
+    let [email, setEmail] = useState('');
+    let [telephone, setTelephone] = useState('');
+    let [gender, setGender] = useState('');
+    let [dateOfBirth, setDateOfBirth] = useState('');
+    let [username, setUsername] = useState('');
+    let [password, setPassword] = useState('');
+    let [confirmedPassword, setConfirmedPassword] = useState('');
+
+    function printState(e) {
+        e.preventDefault();
+        setUserSignUpData({
+            type: type,
+            fullName: fullName,
+            email: email,
+            telephone: telephone,
+            gender: gender,
+            dateOfBirth: dateOfBirth,
+            username: username,
+            password: password,
+            confirmedPassword: confirmedPassword
+        })
+        console.log("Full name: ", fullName);
+        console.log("Email: ", email);
+        console.log("telephone: ", telephone);
+        console.log("gender: ", gender);
+        console.log("object: ", userSignupData);
+    }
+
+    function loadDataToContext() {
+        alert('loading data')
+    }
+
     let [listForms, setListForms] = useState([
         <Fragment>
             <form>
                 <BaseInputRadioButtons title='Type of account' listOfOptions={['Tenant','Landlord','Contractor']} />
-                <BaseInputText title='full name' type='text' />
-                <BaseInputText title='email' type='email' />
-                <BaseInputTelephone title='telephone' />
+                <BaseInputText title='full name' type='text' valueUpdated={ fullName => setFullName(fullName) } />
+                <BaseInputText title='email' type='email' valueUpdated={ email => setEmail(email) }/>
+                <BaseInputText title='telephone' valueUpdated={ telephone => setTelephone(telephone) }/>
             </form>
         </Fragment>,
         <Fragment>
             <form>
-                <BaseInputText title='gender' type='text' />
-                <BaseInputText title='date of birth' type='date' />
+                <BaseInputText title='gender' type='text' valueUpdated={ gender => setGender(gender) } />
+                <BaseInputText title='date of birth' type='date' valueUpdated={ dateOfBirth => setDateOfBirth(dateOfBirth) }/>
             </form>
         </Fragment>,
         <Fragment>
             <form>
-                <BaseInputText title='username' type='text' />
-                <BaseInputText title='password' type='password' />
-                <BaseInputText title='confirm password' type='password' />
+                <BaseInputText title='username' type='text' valueUpdated={ username => setUsername(username) } />
+                <BaseInputText title='password' type='password' valueUpdated={ password => setPassword(password) } />
+                <BaseInputText title='confirm password' type='password' valueUpdated={ confirmedPassword => setConfirmedPassword(confirmedPassword) } />
             </form>
         </Fragment>
     ])
@@ -34,7 +80,8 @@ const TheSignupForm = () => {
     return (
         <div className='the-signup-form'>
             <h2 className='glb-h2'>Create a new account!</h2>
-            <BaseMultipageCard numberOfPages={3} listPages={listForms} />
+            <BaseMultipageCard numberOfPages={3} listPages={listForms} buttonFinishPressed={() => loadDataToContext()} />
+            <button onClick={printState}>Show state</button>
         </div>
     );
 }
