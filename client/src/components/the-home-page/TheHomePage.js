ff6daadb6ae6810c1ca52f49901460e1502c4934
Alexandru Robert <robertalexandru2712@gmail.com>
2020-11-20 13:44:09 +0200

    Added page class to home page

--- client/src/components/the-home-page/TheHomePage.js
+++ client/src/components/the-home-page/TheHomePage.js
@@ -1,9 +1,10 @@
 
 import './TheHomePage.scss';
+import '../../style/classes.scss';
 
 const TheHomePage = () => {
     return (
-        <div className='TheHomePage'>
+        <div className='TheHomePage page'>
             <h1>The Home Page</h1>
         </div>
     );
