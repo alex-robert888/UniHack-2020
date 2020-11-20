140f457178203c1bbe190d318e1efc369252984b
Alexandrescu <ar_alexandrescu@yahoo.com>
2020-11-20 13:54:20 +0200

    mychanges

--- client/src/components/the-home-page/TheHomePage.js
+++ client/src/components/the-home-page/TheHomePage.js
@@ -1,9 +1,9 @@
-
 import './TheHomePage.scss';
+import '../../style/classes.scss';
 
 const TheHomePage = () => {
     return (
-        <div className='TheHomePage'>
+        <div className='TheHomePage page'>
             <h1>The Home Page</h1>
         </div>
     );
