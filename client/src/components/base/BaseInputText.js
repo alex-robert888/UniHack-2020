87b08f91ddcb12fffe3139ed72dfdd0e320cb509
Alexandru Robert <robertalexandru2712@gmail.com>
2020-11-20 17:07:47 +0200

    Before merging with andrei branch

--- client/src/components/base/BaseInputText.js
+++ client/src/components/base/BaseInputText.js
@@ -5,7 +5,9 @@ const BaseInputText = ({title, icon, type}) => {
     return (
         <article className='base-input-text glb-base-input-component'>
             <label htmlFor="input-name" className="glb-base-label">{title}</label>
-            <input type={type} name="input-name" />
+            <input type={type} name="input-name" className="glb-base-input">
+                <img src={icon} alt="icon"/>
+            </input>
         </article>
     );
 }
