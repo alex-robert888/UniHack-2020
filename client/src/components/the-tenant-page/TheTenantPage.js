acb3a4194085aed6fbb9f8bc1bc49d4c3335130d
Alexandru Robert <robertalexandru2712@gmail.com>
2020-11-21 13:04:19 +0200

    Log in refreshed

--- client/src/components/the-tenant-page/TheTenantPage.js
+++ client/src/components/the-tenant-page/TheTenantPage.js
@@ -10,6 +10,7 @@ const TheTenantPage = () => {
     let [listIssues, setListIssues] = useState([
         // ISSUES should be taken from the db for a given user - from localStorage
     ])
+
     return (
         <section className='glb-page main-tenant'>
             <LogOut />
