a79079faa3bea316e254026654829e49e56c60ec
Alexandrescu <ar_alexandrescu@yahoo.com>
2020-11-21 18:39:22 +0200

    Work work

---
+++ client/src/components/base/BaseNotificationButton.js
@@ -0,0 +1,11 @@
+import './BaseNotificationButton.scss';
+
+const BaseNotificationButton = () => {
+    return (
+        <section className='style'>
+            <img src="../../assets/img/icons/notification-icon.svg" className="img-notification"/>
+        </section>
+    );
+}
+
+export default BaseNotificationButton;
