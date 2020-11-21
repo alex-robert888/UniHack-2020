ff591f3865730fc4c788030afbe8b1bf3839aa5d
Alexandru Robert <robertalexandru2712@gmail.com>
2020-11-21 14:27:01 +0200

    Added address comp

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import UserState from './context/user/userState';
 import TheTenantPage from './components/the-tenant-page/TheTenantPage';
 import TheContractorPage from './components/the-contractor-page/TheContractorPage';
 import TheLandlordPage from './components/the-landlord-page/TheLandlordPage';
+import TheAddressForm from './components/the-landlord-page/TheAddressForm'
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
             <Route path='/tenant' exact component={TheTenantPage} />
             <Route path='/contractor' exact component={TheContractorPage} />
             <Route path='/landlord' exact component={TheLandlordPage} />
+            <Route path='/landlord/address' exact component={TheAddressForm} />
           </Switch>
         </div>
       </Router>
